ef458b560dd4eb8bc2cd1eeadbfc115873ba59e5
DoepkerK <156134568+doepkerk@users.noreply.github.com>
2024-03-12 11:39:57 -0400

    Update scriptChartJS.js

--- ChartJS Files/scriptChartJS.js
+++ ChartJS Files/scriptChartJS.js
@@ -78,24 +78,8 @@ const data = {
   datasets: [{
     label: 'Upload/Download Speeds',
     data: [18, 12, 6, 9, 12, 3, 9],
-    backgroundColor: [
-      'rgba(255, 26, 104, 1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)',
-      'rgba(0, 0, 0, 1)'
-    ],
-    borderColor: [
-      'rgba(255, 26, 104, 1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)',
-      'rgba(0, 0, 0, 1)'
-    ],
+    backgroundColor: "#fdb94e",
+    borderColor: "black",
     borderWidth: 1
   }]
 };
