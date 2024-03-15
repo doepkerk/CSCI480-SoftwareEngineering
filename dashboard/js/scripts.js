c514f3d0b245fc422e15410b0d5b0e2f71067a94
DoepkerK <156134568+doepkerk@users.noreply.github.com>
2024-03-15 14:08:19 -0400

    Update scripts.js

--- dashboard/js/scripts.js
+++ dashboard/js/scripts.js
@@ -36,56 +36,82 @@ function updateChart() {
           speeds[i]= speeds[i].toFixed(2);
         }
       }
+
+      let hourlyDownload = [];
+      let hourlyUpload = [];
+      let hourly = [];
+
+      // stores 24 hours worth of data into 3 arrays
+      function getHourly(upload, download, hour) {
+        for(let i=0; i<24;i++) {
+          hourlyUpload.push(upload[i]);
+          hourlyDownload.push(download[i]);
+          hourly.push(hour[i]);
+        };
+
+        console.log(hourlyDownload);
+        console.log(hourly);
+      }
+
       speedConversion(downloadSpeeds);
       speedConversion(uploadSpeeds);
+      getHourly(uploadSpeeds, downloadSpeeds, timestamps);
       console.log(timestamps);
       console.log(downloadSpeeds);
       console.log(uploadSpeeds);
 
+
+
       // CHARTS
 
       // line chart
-      var lineChartOptions = {
-        series: [{
-          name: "MBps",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-        }],
-        chart: {
-          height: 350,
-          type: 'line',
-          zoom: {
-            enabled: false
-          }
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth',
-        },
-        title: {
-          text: 'Internet Speeds',
-          align: 'left'
-        },
-        grid: {
-          row: {
-            colors: ['#212121', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5
+
+      function setupChart(lineData, labels, chartTitle){
+        var lineChartOptions = {
+          series: [{
+            name: "MBps",
+            data: lineData
+          }],
+          chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+              enabled: false
+            }
           },
-        },
-        xaxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-          labels: {
-            style: {
-              colors: "#FFFFFF"
+          dataLabels: {
+            enabled: false
+          },
+          stroke: {
+            curve: 'smooth',
+          },
+          title: {
+            text: chartTitle,
+            align: 'left'
+          },
+          grid: {
+            row: {
+              colors: ['#212121', 'transparent'], // takes an array which will be repeated on columns
+              opacity: 0.5
+            },
+          },
+          xaxis: {
+            categories: labels,
+            labels: {
+              style: {
+                colors: "#FFFFFF"
+              }
             }
           }
-        }
-      };
+        };
 
-      var chart = new ApexCharts(document.querySelector("#line-chart"), lineChartOptions);
-      chart.render();
+        return lineChartOptions;
+      }
 
+      var downloadChart = new ApexCharts(document.querySelector("#download-chart"), setupChart(hourlyDownload, hourly, "Download Speeds"));
+      var uploadChart = new ApexCharts(document.querySelector("#upload-chart"), setupChart(hourlyUpload, hourly, "Upload Speeds"));
+      downloadChart.render();
+      uploadChart.render();
 
     }).catch(error => console.error('Error fetching JSON:', error));
 }
